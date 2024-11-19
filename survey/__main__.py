62f4aac6ffd181470b12b2aab77bf0b02507c2b4
Joschua Thomas Simon-Liedtke (win-10) <joschua@nr.no>
2024-11-19 12:40:01 +0100

    Created analysis function.

--- survey/__main__.py
+++ survey/__main__.py
@@ -6,4 +6,15 @@
   This package is used to analys the results from the survey in the project Tilgjengelige informasjonskapsler, conducted in October and November of 2024.
 """
 
+import os
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
 print("Hello world!")
+
+def analyze_data():
+   
+
+if __name__=='__main__':
+  analyze_data()
