9b1e99dc79d63518cc9f3048f6b72798f95e3f48
Joschua Thomas Simon-Liedtke (win-10) <joschua@nr.no>
2024-11-19 10:26:34 +0100

    Created new package so collect functions to analyze results from survey.

---
+++ survey/__main__.py
@@ -0,0 +1,9 @@
+"""
+  Created on 2024-11-19.
+  
+  Last updated on 2024-11-19.
+  
+  This package is used to analys the results from the survey in the project Tilgjengelige informasjonskapsler, conducted in October and November of 2024.
+"""
+
+print("Hello world!")
