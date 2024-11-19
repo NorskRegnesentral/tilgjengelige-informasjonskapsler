5b1344bbc44e55f5095c91ac0c9c727c9f306ddb
Joschua Thomas Simon-Liedtke (win-10) <joschua@nr.no>
2024-11-19 12:40:52 +0100

    Included basic structure of analysis.

--- survey/__main__.py
+++ survey/__main__.py
@@ -15,6 +15,63 @@ print("Hello world!")
 
 def analyze_data():
    
+   """
+   *** Analysis ***
+   """
+   
+   """
+   1. Country: land
+   
+   Type: Pie chart
+   """
+
+   """
+   2. Age: alder
+   
+   Type: Pie chart
+   """
+   
+   """
+   3. Gender: kjonn
+   
+   Type: Pie chart
+   """
+   
+   """
+   4. Impairment: funksjonsnedsettelse
+   
+   Type: Pie chart
+   """
+   
+   """
+   5. Internet: internettvaner
+   
+   Type: Pie chart
+   """
+   
+   """
+   6. Default choice: default_valg
+   
+   Type: Pie chart
+   """
+   
+   """
+   7. Difficulty (general): vanskelighetsgrad-generell
+   
+   Type: Bar chart
+   """
+   
+   """
+   8. Difficulty (text): vanskelighetsgrad-tekst
+   
+   Type: Bar chart
+   """
+
+   """
+   9. Difficulty (choice): vanskelighetsgrad-generell
+   
+   Type: Bar chart
+   """
 
 if __name__=='__main__':
   analyze_data()
