1bd0c165eebd34e41f9beec19279a878e65dd173
Joschua Thomas Simon-Liedtke (win-10) <joschua@nr.no>
2024-11-25 09:53:17 +0100

    Added options to adjust fig size.

--- survey/visualization.py
+++ survey/visualization.py
@@ -3,24 +3,29 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_data(data,kind,title,save_file="",is_percentage=False):
+import config
+
+def plot_data(data,kind,title,save_file="",is_percentage=False,fig_size=()):
    """
    Returns the results as string.
    """
    
    colors = "Pastel2"
-   fig, ax = plt.subplots()   
-         
+   fig, ax = plt.subplots()
+   
+   if fig_size:
+      fig.set_size_inches(fig_size)   
+
    if kind == "pie":
-      pupsi ='%1.f%%'   
-      data.plot(kind=kind, ax=ax,autopct=pupsi, ylabel="", cmap=colors)
+      pupsi ='%1.f%%'
+      data.plot(kind=kind,subplots=True,ax=ax,autopct=pupsi, ylabel="", cmap=colors)
 
    else: 
       if is_percentage:
          total = data.sum()
          pct = round((data/total) * 100,0)
          data = pct
-         
+      
       data.plot(kind=kind,ax=ax, cmap=colors)
       
       for container in ax.containers:
